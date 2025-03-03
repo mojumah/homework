923d5751ac731bb05eee3231066734d9ffe394ba
Mohammad <mohammad.jumah.uk@gmail.com>
2025-03-03 17:48:51 +0000

    writing if statments and loops in bash

---
+++ if.sh
@@ -0,0 +1,9 @@
+word=monday
+echo $word
+if word=monday
+then
+echo "Write the first letter in capital"
+echo ${word^}
+else
+echo "Don't write the first letter in capital"
+fi
---
+++ loop.sh
@@ -0,0 +1,5 @@
+sentence="the kid got this pot on monday"
+for word in $sentence
+do 
+echo $word
+done
